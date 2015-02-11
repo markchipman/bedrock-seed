1ca9eed8f33b32ca6edef859a6f66192a5c0fd52
David I. Lehn <dlehn@digitalbazaar.com>
2015-02-10 21:08:35 -0500

    Remove unused website role support.

--- configs/roles.js
+++ configs/roles.js
@@ -1,7 +1,5 @@
 var config = require('bedrock').config;
 
-require('./permissions');
-
 var permissions = config.permission.permissions;
 var roles = config.permission.roles;
 
@@ -32,21 +30,13 @@ roles['identity.manager'] = {
     permissions.PUBLIC_KEY_REMOVE.id
   ]
 };
-roles['website.admin'] = {
-  id: 'website.administrator',
-  label: 'Website Administrator',
-  comment: 'This role is used to administer the website.',
-  sysPermission: [permissions.WEBSITE_ADMIN.id]
-};
 
 // admin role contains all permissions
 roles['admin'] = {
   id: 'admin',
   label: 'Administrator',
   comment: 'Role for System Administrator.',
-  sysPermission: [].concat(
-    roles['identity.admin'].sysPermission,
-    roles['website.admin'].sysPermission)
+  sysPermission: [].concat(roles['identity.admin'].sysPermission)
 };
 
 // default registered identity role (contains all permissions for a regular
